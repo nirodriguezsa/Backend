d366e660b73307a8d93e619dc691c965149c601d
root <jjmontoyag@unal.edu.co>
2020-09-28 20:23:06 -0500

    Proyecto Template

---
+++ unadopcion/src/main/java/com/unadopcion/unadopcion/UnadopcionApplication.java
@@ -0,0 +1,18 @@
+package com.unadopcion.unadopcion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class UnadopcionApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(UnadopcionApplication.class, args);
+
+    }
+
+
+}
---
+++ unadopcion/src/main/java/com/unadopcion/unadopcion/modelo/Animal.java
@@ -0,0 +1,6 @@
+package com.unadopcion.unadopcion.modelo;
+
+public class Animal {
+
+    //atributos de la base de datos
+}
